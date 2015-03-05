43b2d121b4c754e5bee97d5f7ddb2db5dd4ff1a5
purinz <purinz00@naver.com>
2015-03-05 19:52:54 +0900

    dddd
    n

--- html/default.js
+++ html/default.js
@@ -1,6 +1,16 @@
 jQuery(function($){
-	$(".gnb>button").on("click", function(){
+	var $gnbBtn = $(".gnb>button");
+
+	$gnbBtn.on("click", function(){
 		//$(".gnb>ul").slideToggle(200);
 		$(this).next().slideToggle(200);
+		
+		if ( $gnbBtn.text() === "메뉴 열기" ){
+			//메뉴 닫기 텍스트 표시
+			$gnbBtn.text("메뉴 닫기")	
+		} else {
+			//메뉴 열기 텍스트 표시 
+			$gnbBtn.text("메뉴 열기")
+		} 
 	});
 });
